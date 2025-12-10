063e1545ada93aa27cba3c9dd2ac65c73af7c2b3
Jeremy-James Noivo <jayjamesnoivo7501@hotmail.com>
2025-12-10 14:19:17 -0500

    Ajout de la fonction afficher_entrees()

--- menu.py
+++ menu.py
@@ -1,7 +1,16 @@
 # Structure de départ (Personne 1)
 def main():
-    print("=== MENU DU RESTAURANT ===")
+    print("---- MENU DU RESTAURANT ----")
+    afficher_entrees()
     # Les autres ajouteront leur code ici
+
+
+def afficher_entrees():
+    print("---- ENTRÉES ----")
+    print("1. Salade César - 8.99$")
+    print("2. Soupe à l'oignon - 6.50$")
+    print("3. Bruschettas maison - 7.25$")
+
 
 if __name__ == "__main__":
     main()
