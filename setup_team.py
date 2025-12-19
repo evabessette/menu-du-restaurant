3979b3fd4873b8f8dbc5c799487b76cfa4c8edd2
evabessette <evabessette@gmail.com>
2025-12-18 21:29:57 -0500

    Create setup_team.py

---
+++ setup_team.py
@@ -0,0 +1,10 @@
+def initialiser_projet():
+    print("=== INITIALISATION DU PROJET ===")
+    print("1. Créer dépôt GitHub")
+    print("2. Cloner sur toutes les machines")
+    print("3. Créer branches :")
+    print("   - git checkout -b feature-Marie-Ève Ferland-Bernier")
+    print("   - git checkout -b feature-Jeremy Noivo")
+    print("   - git checkout -b feature-Jean-Simon Cyr")
+    print("   - git checkout -b feature-Eva Bessette")
+    print("4. Commencer à coder !")
